dccf0bca9941042456b5e407aec0837c49cdb56c
Elías Barrios <elias.barrios@tcpip.tech>
2023-04-10 11:05:55 -0600

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import * as http from "http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'demo';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get("localhost:8085/api/demo/hola").subscribe({
+      next: value => console.log(value),
+      error: err => console.log(err)
+      }
+    )
+  }
+
+
 }
